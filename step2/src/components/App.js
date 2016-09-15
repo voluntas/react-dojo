2a0c5eeba9f077c040709a4d79bc94a545185259
NAKAI Ryosuke <nakai@shiguredo.jp>
2016-09-15 18:43:23 +0900

    Step2 を追加する

---
+++ step2/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world.</h1>
+                <RaisedButton label="Hello, world." />
+            </div>
+        );
+    }
+}
